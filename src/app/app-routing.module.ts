1c61040a239ff368fee8410d840a1c72ab95ed79
ChiaLing <hochialing91@gmail.com>
2019-06-09 14:36:08 +0200

    first attempt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: 'signup', pathMatch: 'full' },
+  { path: 'login', loadChildren: './page/login/login.module#LoginPageModule' },
+  { path: 'signup', loadChildren: './page/signup/signup.module#SignupPageModule' },
+  { path: 'welcome', loadChildren: './page/welcome/welcome.module#WelcomePageModule' },
 ];
 
 @NgModule({
